28e84929b811569cfc79b1de87f06852566a80f7
TatCherOlga <royalty.dai0308@gmail.com>
2024-08-04 20:18:45 +0300

    first_branch

---
+++ src/main/java/com/example/Glovo/JP_H20_Glovo/JpH20GlovoApplication.java
@@ -0,0 +1,14 @@
+package com.example.Glovo.JP_H20_Glovo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpH20GlovoApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(JpH20GlovoApplication.class, args);
+	}
+
+}
